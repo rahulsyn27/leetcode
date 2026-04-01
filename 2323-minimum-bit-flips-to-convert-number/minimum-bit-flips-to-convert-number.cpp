16153e8e36b0feffd5c96030e1c33b6c1a8e910c
RAHUL R <108997053+rahulsyn27@users.noreply.github.com>
2026-04-01 23:50:50 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (26.86%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -13,10 +13,23 @@
 // };
 
 //method 2
+// class Solution {
+// public:
+//     int minBitFlips(int start, int goal) {
+//         // __builtin_popcount is the GCC/Clang equivalent if not on C++20
+//         return __builtin_popcount(start ^ goal);
+//     }
+// };
+
+//method 3 The Classical "Brian Kernighan’s Algorithm"
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        // __builtin_popcount is the GCC/Clang equivalent if not on C++20
-        return __builtin_popcount(start ^ goal);
+        int temp = start ^ goal, count = 0;
+        while(temp > 0){ //this loop only runs as many times as there are set bits.
+            temp &= (temp-1);
+            count++;
+        }
+        return count;
     }
 };
