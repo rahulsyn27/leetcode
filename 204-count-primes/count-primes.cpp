a2b1330b0986f722f07fd99018a4a786f7fc9070
RAHUL R <108997053+rahulsyn27@users.noreply.github.com>
2026-04-07 22:34:53 +0530

    Time: 174 ms (54.80%) | Memory: 12.7 MB (63.82%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,20 +1,51 @@
+// class Solution {
+// public:
+//     int countPrimes(int n) {
+//         if (n < 2)
+//             return 0;
+//         vector<bool> isPrime(n, true);
+//         int x, count = 0;
+//         isPrime[0] = isPrime[1] = false;
+//         for (int i = 2; i < n; i++) {
+//             if (isPrime[i] == true) {
+//                 count++;
+//                 x = 2;
+//                 while (i * x < n) {
+//                     isPrime[i * x++] = false;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+// more efficient method is to check only till p*p
 class Solution {
 public:
     int countPrimes(int n) {
-        if(n < 2) return 0;
-        vector<bool> isPrime(n, true);
-        int x, count = 0;
-        isPrime[0] = isPrime[1] = false;
-        for(int i = 2; i < n; i++){
-            if(isPrime[i] == true) {
-                count++;
-                x = 2;
-                while(i*x < n){
-                    isPrime[i*x++] = false;
+        if (n < 2) return 0;
+
+        vector<bool> prime(n,true);
+        prime[0] = prime[1] = false;
+
+        // Apply Sieve of Eratosthenes
+        for (int p = 2; p * p <= n; p++) {
+            if (prime[p]) {
+
+                // Mark all multiples of p as not prime
+                for (int i = p * p; i < n; i += p) {
+                    prime[i] = false;
                 }
             }
         }
+
+        int count = 0;
+        for (int p = 2; p < n; p++) {
+            if (prime[p]) {
+                count++;
+            }
+        }
+
         return count;
-        
     }
 };
